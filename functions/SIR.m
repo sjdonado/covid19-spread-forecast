91e4e770cafb31b17246d256a83836283a062183
Brian Ramirez <brians@uninorte.edu.co>
2020-05-01 18:29:12 -0500

    refactor: Update SIR function   - Update SIR equations   - Improve perfomance calculation by using ode15s instead of ode45

--- functions/SIR.m
+++ functions/SIR.m
@@ -14,12 +14,11 @@ function [T,X] = SIR(S0,I0,R0,beta,gamma,tmax)
 %   X: vector [3xN] of the target time-histories of the
 %      [susceptible, infectious, recovered] cases
 
-function dx = epi(~,x)
-    dx = zeros(3,1);
-    dx(1) = (-beta*x(1)*x(2)/S0);
-    dx(2) = (+beta*x(1)*x(2)/S0) - gamma*x(2);
-    dx(3) = +gamma*x(2);
+function dx = dx(~,x)
+    dx(1) = -beta*x(1)*x(2);
+    dx(2) = beta*x(1)*x(2) - gamma*x(2);
+    dx(3) = gamma*x(2);
+    dx = [dx(1),dx(2),dx(3)]';
 end
-
-[T,X] = ode45(@epi,[0 tmax],[S0,I0,R0]);
+[T,X] = ode15s(@dx ,[0,tmax],[S0,I0,R0]);
 end
